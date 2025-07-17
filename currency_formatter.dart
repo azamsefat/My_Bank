cfd99d1ac683e077a5a321ceb9e7bfdecf70d87f
azamsefat <soyebazam3@gmail.com>
2025-07-17 23:48:26 +0600

    Delete currency_formatter.dart

--- currency_formatter.dart
+++
@@ -1,6 +0,0 @@
-import 'package:intl/intl.dart';
-
-String formatTaka(double amount) {
-  final formatter = NumberFormat.currency(locale: 'en_BD', symbol: '৳');
-  return formatter.format(amount);
-}
